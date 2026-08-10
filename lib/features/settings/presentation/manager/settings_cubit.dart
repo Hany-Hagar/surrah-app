eada098ccd2960e536b83662ff97332de1663cf0
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-11 01:08:42 +0300

    feat(settings): Add settings cubit and my_bloc_observer files

---
+++ lib/features/settings/presentation/manager/settings_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../model/app_user_pref_model.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+class SettingsCubit extends HydratedCubit<AppUserPref> {
+  SettingsCubit() : super(AppUserPref.standard());
+
+  void changeLanguage(String lang) {
+    emit(state.copyWith(lang: lang));
+  }
+
+  void changeTheme(ThemeMode theme) {
+    emit(state.copyWith(theme: theme));
+  }
+
+  void setFirstTime(bool isFirstTime) {
+    emit(state.copyWith(isFirstTime: isFirstTime));
+  }
+
+  void setNotificationsEnabled(bool enabled) {
+    emit(state.copyWith(notificationsEnabled: enabled));
+  }
+
+  @override
+  AppUserPref? fromJson(Map<String, dynamic> json) {
+    return AppUserPref.fromJson(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(AppUserPref state) {
+    return state.toJson();
+  }
+}
