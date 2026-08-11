5d480c16f97d69580bc66462bbbf355316b2920a
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-11 21:20:42 +0300

    feat(splash): Update native splash screen

--- lib/const/assets.dart
+++ lib/const/assets.dart
@@ -1,4 +1,7 @@
 class Assets {
   // App Assets
   static const String appLogo = 'assets/images/appLogo.png';
+
+  // Splash Screen Assets
+  static const String nativeSplashLogo = 'assets/images/nativeSplashLogo.png';
 }
