8e77e48fc3ec308fece344884b9dcde4b59a66ee
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-12 16:59:30 +0300

    feat(on-boarding): Add on_boarding model

---
+++ lib/features/onBoarding/model/on_boarding_model.dart
@@ -0,0 +1,12 @@
+class OnBoardingModel {
+  final int id;
+  final String image;
+  final String title;
+  final String description;
+  OnBoardingModel({
+    required this.id,
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
