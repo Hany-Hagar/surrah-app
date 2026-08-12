ba1722d02506734a979e907f3513705be4e32314
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-12 16:48:31 +0300

    feat(splash): Add splash logic

--- lib/features/splash/pages/views/splash_view.dart
+++ lib/features/splash/pages/views/splash_view.dart
@@ -1,9 +1,30 @@
 import '../widgets/splash_body.dart';
 import 'package:flutter/material.dart';
 import '../../../../const/assets.dart';
+import '../../../../core/di/server_locator.dart';
+import '../../../settings/presentation/manager/settings_cubit.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 1), () {
+      if (!mounted) return;
+      if (getIt<SettingsCubit>().state.isFirstTime) {
+        // Navigate to onboarding screen
+      } else {
+        // Navigate to home screen
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
