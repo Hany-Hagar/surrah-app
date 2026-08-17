97716c8845bb44d59e9d397de091feb860a954ee
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-17 22:04:52 +0300

    feat(icon-picker): Add app_icon_model file

---
+++ lib/features/iconPicker/models/app_icon_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppIcon {
+  final String id;
+  final IconData icon;
+  final List<String> keywords;
+  const AppIcon({required this.id, required this.icon, required this.keywords});
+}
