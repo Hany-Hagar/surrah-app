1f818b9a42ee753ee1bdd44a7b2a7432f52a7f7d
HanyHagar <hanymagdyhagar@gmail.com>
2026-08-17 23:14:35 +0300

    feat(icon-picker): Add icons data

--- lib/features/iconPicker/models/app_icon_model.dart
+++ lib/features/iconPicker/models/app_icon_model.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppIcon {
   final String id;
-  final IconData icon;
+  final FaIconData icon;
   final List<String> keywords;
   const AppIcon({required this.id, required this.icon, required this.keywords});
 }
